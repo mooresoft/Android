98c7603055a77b1ae86886a04e978348f6a65ad5
“shenshumao” <shen@nbjinque.com>
2017-06-25 10:46:29 +0800

    首次登陆，导航页面初步完成。但更换图片存在问题

--- SplashDemo/app/src/main/java/com/shen/splashdemo/GuideActivity.java
+++ SplashDemo/app/src/main/java/com/shen/splashdemo/GuideActivity.java
@@ -1,173 +1,143 @@
 package com.shen.splashdemo;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.view.View;
-import java.util.List;
-import android.widget.Button;
-import android.widget.ImageView;
 
-import android.view.LayoutInflater;
+
 import java.util.ArrayList;
+import java.util.List;
 
-import com.devilwwj.featureguide.utils.SpUtils;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import com.shen.splashdemo.ViewPagerAdapter;
 
-public class GuideActivity extends AppCompatActivity {
-    private ViewPager vp;
-    private GuideViewPagerAdapter adapter;
-    private List<View> views;
-    private Button startBtn;
+/**
+ *
+ * @{# GuideActivity.java Create on 2013-5-2 下午10:59:08
+ *
+ *     class desc: 引导界面
+ *
+ *     <p>
+ *     Copyright: Copyright(c) 2013
+ *     </p>
+ * @Version 1.0
+ * @Author <a href="mailto:gaolei_xj@163.com">Leo</a>
+ *
+ *
+ */
+public class GuideActivity extends AppCompatActivity implements OnPageChangeListener {
 
-    // 引导页图片资源
-    private static final int[] pics = { R.layout.guid_view1,
-            R.layout.guid_view2, R.layout.guid_view3, R.layout.guid_view4 };
+        private ViewPager vp;
+        private ViewPagerAdapter vpAdapter;
+        private List<View> views;
 
-    // 底部小点图片
-    private ImageView[] dots;
+        // 底部小点图片
+        private ImageView[] dots;
 
-    // 记录当前选中位置
-    private int currentIndex;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guide);
-        views = new ArrayList<View>();
+        // 记录当前选中位置
+        private int currentIndex;
+        Boolean isFirst;
 
-        // 初始化引导页视图列表
-        for (int i = 0; i < pics.length; i++) {
-            View view = LayoutInflater.from(this).inflate(pics[i], null);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.guide_layout);
 
-            if (i == pics.length - 1) {
-                startBtn = (Button) view.findViewById(R.id.btn_login);
-                startBtn.setTag("enter");
-                startBtn.setOnClickListener(this);
-            }
+                // 初始化页面
+                initViews();
 
-            views.add(view);
-
+                // 初始化底部小点
+                initDots();
         }
 
-        vp = (ViewPager) findViewById(R.id.vp_guide);
-        // 初始化adapter
-        adapter = new GuideViewPagerAdapter(views);
-        vp.setAdapter(adapter);
-        vp.setOnPageChangeListener(new PageChangeListener());
+        private void initViews() {
 
-        initDots();
-    }
+                SharedPreferences pref = getSharedPreferences("first",Activity.MODE_PRIVATE);
+                isFirst = pref.getBoolean("status",true);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+                if(!isFirst){
+                        Intent intent = new Intent(this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // 如果切换到后台，就设置下次不进入功能引导页
-        SpUtils.putBoolean(GuideActivity.this, AppConstants.FIRST_OPEN, true);
-        finish();
-    }
+                LayoutInflater inflater = LayoutInflater.from(this);
+                RelativeLayout guideFour = (RelativeLayout) inflater.inflate(R.layout.guide_four, null);
+                guideFour.findViewById(R.id.toMain).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                Intent intent = new Intent(GuideActivity.this,MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                        }
+                });
+                views = new ArrayList<View>();
+                // 初始化引导图片列表
+                views.add(inflater.inflate(R.layout.guide_one, null));
+                views.add(inflater.inflate(R.layout.guide_two, null));
+                views.add(inflater.inflate(R.layout.guide_three, null));
+                views.add(guideFour);
+                // 初始化Adapter
+                vpAdapter = new ViewPagerAdapter(views, this);
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    private void initDots() {
-        LinearLayout ll = (LinearLayout) findViewById(R.id.ll);
-        dots = new ImageView[pics.length];
-
-        // 循环取得小点图片
-        for (int i = 0; i < pics.length; i++) {
-            // 得到一个LinearLayout下面的每一个子元素
-            dots[i] = (ImageView) ll.getChildAt(i);
-            dots[i].setEnabled(false);// 都设为灰色
-            dots[i].setOnClickListener(this);
-            dots[i].setTag(i);// 设置位置tag，方便取出与当前位置对应
+                vp = (ViewPager) findViewById(R.id.viewpager);
+                vp.setAdapter(vpAdapter);
+                // 绑定回调
+                vp.addOnPageChangeListener(this);
         }
 
-        currentIndex = 0;
-        dots[currentIndex].setEnabled(true); // 设置为白色，即选中状态
+        private void initDots() {
+                LinearLayout ll = (LinearLayout) findViewById(R.id.ll);
 
-    }
+                dots = new ImageView[views.size()];
 
-    /**
-     * 设置当前view
-     *
-     * @param position
-     */
-    private void setCurView(int position) {
-        if (position < 0 || position >= pics.length) {
-            return;
-        }
-        vp.setCurrentItem(position);
-    }
+                // 循环取得小点图片
+                for (int i = 0; i < views.size(); i++) {
+                        dots[i] = (ImageView) ll.getChildAt(i);
+                        dots[i].setEnabled(true);// 都设为灰色
+                }
 
-    /**
-     * 设置当前指示点
-     *
-     * @param position
-     */
-    private void setCurDot(int position) {
-        if (position < 0 || position > pics.length || currentIndex == position) {
-            return;
-        }
-        dots[position].setEnabled(true);
-        dots[currentIndex].setEnabled(false);
-        currentIndex = position;
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getTag().equals("enter")) {
-            enterMainActivity();
-            return;
+                currentIndex = 0;
+                dots[currentIndex].setEnabled(false);// 设置为白色，即选中状态
         }
 
-        int position = (Integer) v.getTag();
-        setCurView(position);
-        setCurDot(position);
-    }
+        private void setCurrentDot(int position) {
+                if (position < 0 || position > views.size() - 1
+                        || currentIndex == position) {
+                        return;
+                }
 
+                dots[position].setEnabled(false);
+                dots[currentIndex].setEnabled(true);
 
-    private void enterMainActivity() {
-        Intent intent = new Intent(GuideActivity.this,
-                SplashActivity.class);
-        startActivity(intent);
-        SpUtils.putBoolean(GuideActivity.this, AppConstants.FIRST_OPEN, true);
-        finish();
-    }
+                currentIndex = position;
+        }
 
-    private class PageChangeListener implements OnPageChangeListener {
         // 当滑动状态改变时调用
         @Override
-        public void onPageScrollStateChanged(int position) {
-            // arg0 ==1的时辰默示正在滑动，arg0==2的时辰默示滑动完毕了，arg0==0的时辰默示什么都没做。
-
+        public void onPageScrollStateChanged(int arg0) {
         }
 
-        // 当前页面被滑动时调用
+        // 当当前页面被滑动时调用
         @Override
-        public void onPageScrolled(int position, float arg1, int arg2) {
-            // arg0 :当前页面，及你点击滑动的页面
-            // arg1:当前页面偏移的百分比
-            // arg2:当前页面偏移的像素位置
-
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
         }
 
         // 当新的页面被选中时调用
         @Override
-        public void onPageSelected(int position) {
-            // 设置底部小点选中状态
-            setCurDot(position);
+        public void onPageSelected(int arg0) {
+                // 设置底部小点选中状态
+                setCurrentDot(arg0);
         }
 
-    }
 }
